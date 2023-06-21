3871cd125866558c8c370fac90f279b4c7b0f9ef
Trixidrom <trixidrom@gmail.com>
2023-06-21 15:59:02 +1100

    - Load URL (Добавил AndroidView с WebView)

--- app/src/main/java/com/example/webviewexample/MainActivity.kt
+++ app/src/main/java/com/example/webviewexample/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.webviewexample
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.webviewexample.ui.theme.WebViewExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +21,7 @@ class MainActivity : ComponentActivity() {
             WebViewExampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    WebViewPage("https://www.boltuix.com/")
                 }
             }
         }
@@ -27,14 +29,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun WebViewPage(url: String){
+    // Добавление WebView внутрь AndroidView
+    // с макетом во весь экран
+    AndroidView(factory = {context ->
+        WebView(context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WebViewExampleTheme {
-        Greeting("Android")
-    }
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    }, update = {
+        it.loadUrl(url)
+    })
 }
